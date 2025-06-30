d29ade398c8c438664961238065dec69b6c893d4
HoangPhuc08022004z <phucgit08022004@gmail.com>
2025-07-01 01:57:25 +0700

    update home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router';
 import Webcam from 'react-webcam';
+import picperfect from "../assets/picperfect.png.png"
 
 const videoConstraints = {
-  width: 320,
-  height: 240,
+  width: 1000,
+  height: 1000,
   facingMode: 'user',
 };
 
@@ -15,29 +16,25 @@ const Home = () => {
   };
 
   return (
-    <section className="min-h-screen flex items-center justify-center bg-white">
-      <div
-        onClick={handleStart}
-        className="flex flex-col items-center justify-center text-center space-y-4 cursor-pointer"
-      >
-        {/* Logo */}
-        <div className="text-xl text-gray-700">Logo shop</div>
-
-        {/* Webcam */}
-        <div className="relative w-[320px] h-[240px]">
+   <section className="min-h-screen flex items-center justify-center bg-[#FFFBEA]">
+      <div className="flex flex-col items-center justify-center text-center space-y-6 cursor-pointer">
+        <img src={picperfect} alt="Logo" className="w-[60%] max-w-md h-auto bg-[#FFFBEA] p-2 rounded-lg"/>
+        <div className="w-[1200px] h-[700px] relative">
           <Webcam
             audio={false}
             screenshotFormat="image/jpeg"
             videoConstraints={videoConstraints}
-            className="w-full h-full object-cover rounded-md"
+            className="w-full h-full object-cover rounded-lg shadow-md"
           />
         </div>
+    <button onClick={handleStart} className="px-8 py-3 border-2 border-[#1F3A63] text-[#1F3A63] font-bold text-3xl rounded-full hover:bg-[#1F3A63]/10 transition duration-200 mt-4">
+        Chạm để bắt đầu
+    </button>
 
-        {/* Text */}
-        <h1 className="text-4xl font-bold text-black">Chạm để bắt đầu</h1>
+
+
       </div>
     </section>
-
   );
 };
 
